cf5b4b95ca7a32d9ab9837c9027aa37a202377ab
Diallo Alpha <jackobdiallo@gmail.com>
2019-04-14 19:00:52 +0200

    add api e-com

--- src/app/entities/product.ts
+++ src/app/entities/product.ts
@@ -1,10 +1,13 @@
 
-export interface Produit {
+export interface Product {
     id:number,
     name:string,
     price:string,
     description:string,
     category:string,
     marque:string,
+    images?: Array<Object>;
+    images_tags?: Array<Object>;
     imageURI: string
+    qty?:number;
 }
