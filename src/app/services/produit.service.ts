b35ef5b36333624cd5885933d24fea0e8905d220
Diallo Alpha <jackobdiallo@gmail.com>
2019-04-14 15:12:46 +0200

    add api

--- src/app/services/produit.service.ts
+++ src/app/services/produit.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders, HttpClientJsonpModule } from '@angular/common/http';
 import { Produit } from '../entities/produit';
 
 import { Observable, of } from 'rxjs';
@@ -11,11 +11,43 @@ import { map, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ProduitService {
-  produits:Produit[]
+  produits:Produit[];
 
+  private url: string;
+  private apiKey: string;
+  private format: string;
+  private filterRelease: string;
+  private httpOptions: Object;
   
-  constructor(private httpClient: HttpClient) {
+
+  private year: string;
+
+
+  httpParams = {
+
   }
+  constructor(private http: HttpClient, private jsonp: HttpClientJsonpModule) {
+    this.url = "https://www.giantbomb.com/api/";
+    this.apiKey = "393b6596b4dc77b7c17ae66bd42cefe0b52c106f"
+    this.format = "jsonp"
+    this.year = "2018"
+    this.filterRelease = "field:2018-01-01|2018-31-12&sort=original_release_date:desc"
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Headers": "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN",
+        'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+      }),
+      params: new HttpParams()
+        .set("api_key", this.apiKey)
+        .set('format', this.format)
+
+    };
+  }
+
 
   /**method pour recupérer tous les produits */   
   getAllProduit() {
